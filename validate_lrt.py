b96ddcfbce4f9046144712868e00bf5b614cede8
Luis Gabriel Hernandez <lghernandez@gmail.com>
2021-03-11 01:30:50 -0500

    New and tested version of validate_lrt.py

--- validate_lrt.py
+++ validate_lrt.py
@@ -1,34 +1,27 @@
 from constants import (
     REMOTE_LRT_PATH,
     LOG_PATH,
+    VALIDATE_LRT_PATH,
+    VSRS,
 )
-from myfunctions import (
+from functions_validate_lrt import (
     gunzip_lrt,
     validate_lrt_all,
     logging,
-    initiate_logfile,
+    create_custom_logger,
     validate_lrt_format,
+    remove_file_by_extension,
 )
-from sftp_socks import download_lrt_all
+from sftp_socks import download_lrt_all_with_logger
+import os
 
-local_path = "D:\\TBS\\Archivos LRTs SRs Clientes\\All-LRTs"
-vsr_host = "192.168.179.7"
-
-vsrs = {
-    "vsrmiabr3": "192.168.229.181",
-    "vsrmiatc3": "192.168.229.218",
-    "vsrloneq3": "192.168.229.56",
-    "vsralhta3": "192.168.229.82",
-    "vsrhkgeq3": "192.168.179.6",
-    "vsrhkgeq4": "192.168.179.7",
-}
-
-initiate_logfile("Validate_LRT", LOG_PATH)
-
-download_lrt_all(vsr_host, local_path, REMOTE_LRT_PATH)
-
-list_LRT = validate_lrt_all(local_path)
-
-for lrt in list_LRT:
-    lrt_1 = gunzip_lrt(lrt)
-    validate_lrt_format(lrt_1)
+for vsr_name, vsr_ip in VSRS.items():
+    vsr_path = os.path.join(VALIDATE_LRT_PATH, vsr_name)
+    remove_file_by_extension(vsr_path, ".xml")
+    my_logger = create_custom_logger(vsr_name, LOG_PATH)
+    download_lrt_all_with_logger(vsr_ip, vsr_path, REMOTE_LRT_PATH, my_logger)
+    list_LRT = validate_lrt_all(vsr_path, my_logger)
+    for lrt in list_LRT:
+        lrt_1 = gunzip_lrt(lrt, my_logger)
+        if lrt_1 is not None:
+            validate_lrt_format(lrt_1, my_logger)
