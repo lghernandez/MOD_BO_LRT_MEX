b074412c9ed500614e2b0774b1a2cf3134742c45
Luis Gabriel Hernandez <lghernandez@gmail.com>
2021-03-13 14:31:12 -0500

    Add function create_message_with_attachment to email_notification.py

--- email_notification.py
+++ email_notification.py
@@ -5,6 +5,10 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from apiclient import errors
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart, MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+import mimetypes
 import base64
 from google.oauth2 import service_account
 import os
@@ -24,6 +28,56 @@ def create_message(sender, to, subject, message_text):
     message["to"] = to
     message["from"] = sender
     message["subject"] = subject
+    return {"raw": base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def create_message_with_attachment(sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = "application/octet-stream"
+    main_type, sub_type = content_type.split("/", 1)
+    if main_type == "text":
+        fp = open(file, "rb")
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == "image":
+        fp = open(file, "rb")
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == "audio":
+        fp = open(file, "rb")
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, "rb")
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header("Content-Disposition", "attachment", filename=filename)
+    message.attach(msg)
+
     return {"raw": base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
 
@@ -50,10 +104,6 @@ def send_message(service, user_id, message):
 def service_account_login():
     # If modifying these scopes, delete the file token.json.
     SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
-
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
