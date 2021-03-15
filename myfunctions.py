38ac3b13fb536e3d7f74b19a90b10878723807ae
Luis Gabriel Hernandez <lghernandez@gmail.com>
2021-03-14 21:17:37 -0500

    Add functions validate_lrt_B_with_logger, gunzip_lrt_with_logger, gzip_lrt_with_logger, modify_lrt_sag_with_logger to myfunctions.py

--- myfunctions.py
+++ myfunctions.py
@@ -32,6 +32,17 @@ def validate_lrt_B(dir_lrt):
     return list_out
 
 
+def validate_lrt_B_with_logger(dir_lrt, logger_name):
+    list_in = os.listdir(path=dir_lrt)
+    list_out = []
+    for lrt in list_in:
+        if lrt.startswith("B") and lrt.endswith(".gz"):
+            list_out.append(lrt)
+            logger_name.info("Successfully obtained LRT: {}".format(lrt))
+    logger_name.info("Total LRTs obtained: {}\n".format(len(list_out)))
+    return list_out
+
+
 def validate_lrt_all(dir_lrt):
     list_in = os.listdir(path=dir_lrt)
     list_out = []
@@ -55,12 +66,37 @@ def gunzip_lrt(input_file):
     return output_file
 
 
+def gunzip_lrt_with_logger(input_file, logger_name):
+    output_file = input_file.rstrip(".gz")
+    with gzip.open(input_file, "rt") as fin:
+        with open(output_file, "w") as fout:
+            fout.write(fin.read())
+    logger_name.info(
+        "Successfully gunzip file {} to file {}".format(input_file, output_file)
+    )
+    os.remove(input_file)
+    return output_file
+
+
 def gzip_lrt(input_file):
     output_file = input_file + ".gz"
     with open(input_file, "r") as fin:
         with gzip.open(output_file, "wt") as fout:
             fout.write(fin.read())
     logging.info("Successfully gzip file {} to file {}".format(input_file, output_file))
+
+    os.remove(input_file)
+    return output_file
+
+
+def gzip_lrt_with_logger(input_file, logger_name):
+    output_file = input_file + ".gz"
+    with open(input_file, "r") as fin:
+        with gzip.open(output_file, "wt") as fout:
+            fout.write(fin.read())
+    logger_name.info(
+        "Successfully gzip file {} to file {}".format(input_file, output_file)
+    )
 
     os.remove(input_file)
     return output_file
@@ -86,6 +122,28 @@ def modify_lrt_sag(file_xml, tcontext, from_sag, to_sag):
     with open(file_xml, "wb") as f:
         f.write(xml_data)
     logging.info("Successfully modified LRT {}".format(file_xml))
+
+
+def modify_lrt_sag_with_logger(file_xml, tcontext, from_sag, to_sag, logger_name):
+    tree = etree.parse(file_xml)
+    root = tree.getroot()
+    n = 0
+
+    for element in root.iter("next"):
+        if to_sag not in element.text:
+            if tcontext in element.text:
+                element.text = element.text.replace(from_sag, to_sag)
+                n += 1
+
+    logger_name.info("In total has been modified {} entries".format(n))
+
+    xml_data = etree.tostring(
+        tree, pretty_print=True, xml_declaration=True, encoding="UTF-8", standalone=True
+    )
+
+    with open(file_xml, "wb") as f:
+        f.write(xml_data)
+    logger_name.info("Successfully modified LRT {}".format(file_xml))
 
 
 def validate_lrt_format(lrt):
